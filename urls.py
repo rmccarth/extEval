8c1b4b1a991a26f37ee0fea7712eb23a71c9cda4
Rob <36937649+rmccarth@users.noreply.github.com>
2018-11-30 06:31:50 -0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -4,8 +4,8 @@ def getURL():
 
 	data = pandas.read_csv("urls.csv", header=0)  #Create pandas DataFrame by reading top-1m sites csv
 	col_a = list(data.number)		      
-	col_b = list(data.url[0:9])		      #grab URL column and first 9 links from DataFrame
-	return col_b				      #THIS METHOD STILL NEEDS TO VISIT EACH LINK AND PULL FIRST 5 LINKS AND ADD IT TO A LIST ELEMENT TO RETURN
+	col_b = list(data.url[0:5001])		      #grab URL column and first 5000 links from DataFrame. This executiion was done in segments by team members.
+	return col_b				      
 
 def main():
 	getURL()
